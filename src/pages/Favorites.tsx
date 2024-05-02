4da3e53529e34e5fbd32a87b40f34cdb1a47548c
Feliphito <138700011+Feliphito@users.noreply.github.com>
2024-05-02 15:57:14 -0400

    Update Favorites.tsx

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -6,11 +6,13 @@ export default function Favorites() {
 
   const items = getItems();
   return (
-    <div className="bg-black h-screen flex-auto">
+    <div className="bg-black min-h-screen">
+    <div className="bg-black h-full w-full grid grid-cols-6 rounded">
       {(items?.length as number) > 0 && items !== undefined ? (
         items?.map((data) => {
           return (
-            <div className="bg-black max-w-sm rounded overflow-hidden shadow-lg mt-2 ml-2 text-white">
+
+            <div key={data.id} className="bg-black max-w-sm rounded overflow-hidden shadow-lg mt-2 ml-2 text-white">
               <img
                 src={`${image}${data?.poster_path}`}
                 alt={data?.title ?? data?.name}
@@ -20,8 +22,9 @@ export default function Favorites() {
           );
         })
       ) : (
-        <div>NO tienes Favoritos</div>
+        <div className=" text-white text-center ">No tienes Favoritos</div>
       )}
+    </div>
     </div>
   );
 }
